b27534bc2379a3c617b5cae468c8be8e40ec7e29
Benjamin Walkenhorst <krylon@posteo.de>
2026-08-31 10:42:11 +0200

    model - add timestamps to Step

--- model/model.go
+++ model/model.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 31. 08. 2026 by Benjamin Walkenhorst
 // (c) 2026 Benjamin Walkenhorst
-// Time-stamp: <2026-08-31 10:25:00 krylon>
+// Time-stamp: <2026-08-31 10:39:19 krylon>
 
 // Package model defines data types used throughout the application
 package model
@@ -15,10 +15,12 @@ import (
 
 // Step defines a command to be executed as part of a Job.
 type Step struct {
-	Seq       int64 // ???
-	Predicate predicate.ID
-	Command   string
-	Status    int
+	Seq          int64 // ???
+	Predicate    predicate.ID
+	Command      string
+	TimeStarted  time.Time
+	TimeFinished time.Time
+	Status       int
 }
 
 // Job is a sequence of commands to be exected.
